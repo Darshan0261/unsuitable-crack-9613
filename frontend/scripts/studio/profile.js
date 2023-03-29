47224e4a952ada9e62bffafaa0924f8be9e2dce0
Ananyakumarisingh <ananyakumarisingh77@gmail.com>
2023-03-29 21:02:08 +0530

    profile page responsiveness

--- frontend/scripts/studio/profile.js
+++ frontend/scripts/studio/profile.js
@@ -1,7 +1,9 @@
 function editProfileForm() {
     document.querySelector(".bg-model").style.display = 'flex';
-
+    document.querySelector("body").style.overflow = "hidden";
+    
 };
 function closeProfileForm() {
     document.querySelector(".bg-model").style.display = 'none';
+    document.querySelector("body").style.overflow = "scroll";
 };
