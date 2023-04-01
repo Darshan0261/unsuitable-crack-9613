4870f9bd54e9d8dc4a3e274daaa4cafa9e6cae20
Darshan <darshannas11@gmail.com>
2023-04-01 16:40:00 +0530

    User and Studio update routes added

--- backend/routes/Studio.router.js
+++ backend/routes/Studio.router.js
@@ -118,6 +118,59 @@ studioRouter.post('/logout', authentication, studioAuth, async (req, res) => {
     }
 })
 
+studioRouter.patch('/update/:id', authentication, studioAuth, async (req, res) => {
+    const { token } = req.body;
+    const studio_id = req.params['id'];
+    if (token.id != studio_id) {
+        return res.status(401).send({ message: 'Access Denied' });
+    }
+    const payload = req.body;
+    if (payload.password) {
+        return res.status(400).send({ message: 'Bad Request - Wrong Request' });
+    }
+    try {
+        await StudioModel.findOneAndUpdate({ _id: studio_id }, payload);
+        return res.send({ message: 'Profile Updated Sucessfully' })
+    } catch (error) {
+        return res.status(501).send({message: error.message})
+    }
+})
+
+studioRouter.patch('/update/password/:id', authentication, studioAuth, async (req, res) => {
+    const { token } = req.body;
+    const studio_id = req.params['id'];
+    if (token.id != studio_id) {
+        return res.status(401).send({ message: 'Access Denied' });
+    }
+    const { old_password, new_password } = req.body;
+    if (!old_password || !new_password) {
+        return res.status(428).send({ message: 'Password Required' })
+    }
+    try {
+        const user = await StudioModel.findOne({ _id: studio_id });
+        bcrypt.compare(old_password, user.password, function (err, result) {
+            if (err) {
+                return res.status(501).send({ message: err.message });
+            }
+            if (!result) {
+                return res.status(403).send({ message: "Password does not match" })
+            }
+            bcrypt.hash(new_password, +process.env.saltRounds, async function (err, hashedPass) {
+                if (err) {
+                    return res.status(501).send({ message: err.message });
+                }
+                try {
+                    await StudioModel.findOneAndUpdate({ _id: studio_id }, { password: hashedPass });
+                    return res.send({ message: 'Password Changed Sucessfully' })
+                } catch (error) {
+                    return res.status(501).send({ message: error.message });
+                }
+            });
+        });
+    } catch (error) {
+        return res.status(501).send({ message: error.message });
+    }
+})
 
 // Get slots booked for date
 studioRouter.post('/slots/:id', studioIdValidator, bookingDayValidator, async (req, res) => {
@@ -132,7 +185,6 @@ studioRouter.post('/slots/:id', studioIdValidator, bookingDayValidator, async (r
         return res.status(501).send({ message: error.message })
     }
 })
-
 
 // Book slot body = date, start_time, end_time, params = id : studio_id
 studioRouter.post('/slots/book/:id', authentication, userAuth, studioIdValidator, bookingDayValidator, bookingTimeValidator, bookingSlotValidator, async (req, res) => {
