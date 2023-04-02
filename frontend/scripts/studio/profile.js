9fa7329927151046d1758b47bd71eac2dcf172a5
Ananyakumarisingh <ananyakumarisingh77@gmail.com>
2023-04-02 14:05:49 +0530

    profile img changes

--- frontend/scripts/studio/profile.js
+++ frontend/scripts/studio/profile.js
@@ -44,7 +44,7 @@ async function fetchStudio() {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
-          Authorization: `Bearer ${sessionStorage.getItem("access_token")}`,
+          Authorization: localstorage.getItem("token"),
         },
       }
     );
@@ -67,7 +67,7 @@ function displayProfileDetails(data) {
   address.innerText = `${data.street} ${data.city}, ${data.state}`;
   startTime.innerText = data.start_time;
   endTime.innerText = data.end_time;
-  profile_img.innerHTML = `<img src=${data.profile_image} />`;
+  profile_img.innerHTML = `<img width="90%" style="margin-left: 5%;" src=${data.profile_image} />`;
   price.innerText = data.price;
   city.innerText = data.city;
 }
