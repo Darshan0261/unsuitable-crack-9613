390437805e82e328da5034c38a504a6e978d3d51
Darshan <darshannas11@gmail.com>
2023-04-02 18:01:17 +0530

    Small Changes made

---
+++ frontend/scripts/studio/apointments.js
@@ -0,0 +1,83 @@
+// const tableBody = document.querySelector('tbody');
+
+let appointments = [];
+const baseURL = 'https://erin-shiny-lizard.cyclic.app';
+
+async function fetchAppointments() {
+  let res = await fetch(`${baseURL}/appointment`)
+  if (res.ok) {
+    let data = await res.json()
+    appointments = data;
+    fetchUserDetails()
+  } else {
+    alert('Something Went Wrong')
+  }
+
+}
+
+async function fetchUserDetails() {
+  appointments.forEach( async (app) => {
+    const user_id = app.user_id;
+    let res = await fetch(`${baseURL}/users/${user_id}`)
+    if(res.ok) {
+      
+    }
+  })
+}
+// function displayAppointment(data){
+//     tableBody.innerHTML = data.map((item,index)=>{
+//         return `
+//         <tr id=${item.id}>
+//         <td>${}</td>
+//         <td>{item}</td>
+//         <td>2023-04-20</td>
+//         <td>1:00 PM</td>
+//         <td>5:00 PM</td>
+//         <td><button class="custom-btn btn-3"><span>Accept</span></button></td>
+//         <td><button class="custom-btn btn-5"><span>Reject</span></button></td>
+//       </tr>
+//         `
+//     }).join(" ")
+// }
+
+// =================>
+let acceptBtns = document.querySelectorAll(".accept");
+let rejectBtns = document.querySelectorAll(".reject");
+
+acceptBtns.forEach((el) => {
+  el.addEventListener("click", () => {
+    el.innerHTML = "<b>ACCEPTED</b>";
+  });
+});
+
+rejectBtns.forEach((el) => {
+  el.addEventListener("click", () => {
+    el.innerHTML = "<b>REJECTED</b>";
+    el.previousElementSibling.innerHTML = "-"
+    el.parentElement.classList.add('rejected')
+
+
+    // deleteAppointmetn(el.parentElement.id)
+  });
+});
+
+async function deleteAppointmetn(id) {
+  await fetch(`/url/${id}`, {
+    method: "Post",
+    authorization: localStorage.getItem("token"),
+    body: id,
+  });
+  if (res.ok) {
+    alert("Appointment Deleted");
+  } else alert("Something went wrong while delting the appointment");
+}
+async function acceptAppointment(id) {
+  await fetch(`/url/${id}`, {
+    method: "Post",
+    authorization: localStorage.getItem("token"),
+    body: id,
+  });
+  if (res.ok) {
+    alert("Appointment Accepted");
+  } else alert("Something went wrong while accepting the appointment");
+}
